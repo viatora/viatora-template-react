2067de6504cb2c5c201ab27874ca1d728b6be20b
maxitect <maxime.downe@gmail.com>
2024-10-29 12:52:44 +0000

    style: added favicon and styling for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 export default function Header() {
     return (
         <header>
+            <img src="../../assets/favicon.svg" alt="Website logo" />
             <h1>Website Title</h1>
             <Navbar />
         </header>
